fd9f1e1cff2118a8610b55cc42e85d6395d7ebb9
Pattarawat Chormai <goonnow@rocketmail.com>
2014-01-11 00:57:07 +0700

    Update syntax

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,6 @@
 if Rails.env.staging? || Rails.env.production?
-  exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s}
 else
-  exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
+  WickedPdf.config = { :exe_path => '/usr/bin/wkhtmltopdf'}
 end
+
