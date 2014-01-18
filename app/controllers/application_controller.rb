b8eb39b0777e135b37d68ca0e630d771f3832ed1
Pattarawat Chormai <goonnow@rocketmail.com>
2014-01-18 17:28:32 +0700

    Update usability and change logo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,13 @@ class ApplicationController < ActionController::Base
     def certificate
         @info = params
 
+        $birthdate_year_AD = params['birthdate-year'].to_i - 543;
+        $ordained_year_AD = params['ordained-year'].to_i - 543;
+
+        @info[:birthdate] = "#{$birthdate_year_AD}-#{params['birthdate-month']}-#{params['birthdate-date']}"
+        @info['ordained_datetime'] = "#{$ordained_year_AD}-#{params['ordained-month']}-#{params['ordained-date']} #{params['ordained-hour']}:#{params['ordained-minues']}"
+        puts '>>>> '+@info['ordained_datetime'];
+
         render  :pdf => "monk-certificate",
                 :encoding => 'UTF-8',
                 :show_as_html => ( params[:format] != 'pdf' ),
